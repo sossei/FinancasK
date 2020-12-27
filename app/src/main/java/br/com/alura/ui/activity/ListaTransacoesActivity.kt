fe1f3bf07cd84c44cb7b240244bb496dadefa784
Vinicius Sossei Sakugawa <viny.sossei@gmail.com>
2020-12-26 21:40:39 -0300

    refactor: fp3-aula3 = lazy

--- app/src/main/java/br/com/alura/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/ui/activity/ListaTransacoesActivity.kt
@@ -16,13 +16,13 @@ import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : Activity() {
     private val transacoes: MutableList<Transacao> = mutableListOf()
-    private lateinit var viewDaActivity : View
+    private val viewDaActivity by lazy {
+        window.decorView
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
-
-        viewDaActivity = window.decorView
 
         configuraResumo()
         configuraLista()
